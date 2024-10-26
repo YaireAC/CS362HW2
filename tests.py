2ed7ef7262c84d02cda2bd1a02d26529d3453552
Yaire Aguilar <114464751+YaireAC@users.noreply.github.com>
2024-10-25 22:34:39 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,5 +5,6 @@ from contrived_func import contrived_func
 
 class TestContrivedFunc(unittest.TestCase):
 
-    def test_condition_a_true(self):
+    def test1(self):
+        """a is true"""
         contrived_func(10)
