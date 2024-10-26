0943c6d5abe81c59273fe971b80d941663c55c16
Yaire Aguilar <114464751+YaireAC@users.noreply.github.com>
2024-10-25 22:40:20 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,10 +1,31 @@
 import unittest
-import math
 from contrived_func import contrived_func
 
-
 class TestContrivedFunc(unittest.TestCase):
+   
+    def test_case_1(self):
+        contrived_func(-3)  # a=True, b=True, c=True, d=True
 
-    def test1(self):
-        """a is true"""
-        contrived_func(10)
+    def test_case_2(self):
+        contrived_func(5)   # a=True, b=True, c=True, d=False
+
+    def test_case_3(self):
+        contrived_func(20)  # a=True, b=True, c=False, d=False
+
+    def test_case_4(self):
+        contrived_func(6)   # a=True, b=False, c=True, d=False
+
+    def test_case_5(self):
+        contrived_func(3)   # a=False, b=True, c=True, d=False
+
+    def test_case_6(self):
+        contrived_func(2)   # a=False, b=False, c=True, d=False
+
+    def test_case_7(self):
+        contrived_func(-1)  # a=False, b=True, c=False, d=True
+
+    def test_case_8(self):
+        contrived_func(0)   # a=False, b=False, c=False, d=False
+
+if __name__ == '__main__':
+    unittest.main()
