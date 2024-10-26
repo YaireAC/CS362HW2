9117e922342e52deb5dd63486cf4d2263cd9a2a4
Yaire Aguilar <114464751+YaireAC@users.noreply.github.com>
2024-10-25 21:43:42 -0700

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,9 @@
+import unittest
+import math
+from contrived_func import contrived_func
+
+
+class TestContrivedFunc(unittest.TestCase):
+
+    def test_condition_a_true(self):
+        contrived_func(10)
