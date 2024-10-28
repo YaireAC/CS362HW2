cf1e7d036113fd70f98b928048141a6af7f63d24
Yaire Aguilar <114464751+YaireAC@users.noreply.github.com>
2024-10-27 18:52:49 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,34 +4,46 @@ from contrived_func import contrived_func
 class TestContrivedFunc(unittest.TestCase):
    
     def test_case_1(self):
-        contrived_func(-3)  # a=True, b=True, c=True, d=True
+        contrived_func(-3) 
 
     def test_case_2(self):
-        contrived_func(5)   # a=True, b=True, c=True, d=False
+        contrived_func(5)   
 
     def test_case_3(self):
-        contrived_func(20)  # a=True, b=True, c=False, d=False
+        contrived_func(20)  
 
     def test_case_4(self):
-        contrived_func(10)  # a=True, b=False, c=True, d=False
+        contrived_func(10) 
 
     def test_case_5(self):
-        contrived_func(7)   # a=False, b=True, c=True, d=False
+        contrived_func(7) 
 
     def test_case_6(self):
-        contrived_func(2)   # a=False, b=False, c=True, d=False
+        contrived_func(2)   
 
     def test_case_7(self):
-        contrived_func(-1)  # a=False, b=True, c=False, d=True
+        contrived_func(-1) 
 
     def test_case_8(self):
-        contrived_func(3)   # a=False, b=False, c=False, d=False
+        contrived_func(3)   
        
     def test_case_9(self):
         contrived_func(200)
 
     def test_case_11(self):
         contrived_func(50)
+       
+    def test_case_11(self):
+        contrived_func(3)
+
+   def test_case_11(self):
+        contrived_func(18)
+
+   def test_case_11(self):
+        contrived_func(24)
+
+   def test_case_11(self):
+        contrived_func(8)
 
 if __name__ == '__main__':
     unittest.main()
