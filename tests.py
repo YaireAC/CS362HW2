4a752a76b59c228c7c24f120b7949f82756f0dda
Yaire Aguilar <114464751+YaireAC@users.noreply.github.com>
2024-10-27 19:21:35 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,28 +6,26 @@ class TestContrivedFunc(unittest.TestCase):
     def test_case_1(self):
         contrived_func(-3)
 
-    def test_case_1(self):
+    def test_case_2(self):
         contrived_func(-2)
 
-    def test_case_1(self):
+    def test_case_3(self):
         contrived_func(8)
 
-    def test_case_1(self):
+    def test_case_4(self):
         contrived_func(11)
 
-    def test_case_1(self):
+    def test_case_5(self):
         contrived_func(18)
 
-    def test_case_1(self):
+    def test_case_6(self):
         contrived_func(21)
 
-    def test_case_1(self):
+    def test_case_7(self):
         contrived_func(22)
 
-     def test_case_1(self):
+    def test_case_8(self):
         contrived_func(25)
-
-
 
 if __name__ == '__main__':
     unittest.main()
