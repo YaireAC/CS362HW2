635b4ad1c3294321a010261bf2fc72573efa07a8
Yaire Aguilar <114464751+YaireAC@users.noreply.github.com>
2024-10-27 19:20:41 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -3,65 +3,30 @@ from contrived_func import contrived_func
 
 class TestContrivedFunc(unittest.TestCase):
 
-    def test_case_11(self):
+    def test_case_1(self):
+        contrived_func(-3)
+
+    def test_case_1(self):
+        contrived_func(-2)
+
+    def test_case_1(self):
+        contrived_func(8)
+
+    def test_case_1(self):
         contrived_func(11)
 
-    def test_case_12(self):
-        contrived_func(12)
-
-    def test_case_13(self):
-        contrived_func(13)
-
-    def test_case_14(self):
-        contrived_func(14)
-
-    def test_case_15(self):
-        contrived_func(15)
-
-    def test_case_16(self):
-        contrived_func(16)
-
-    def test_case_17(self):
-        contrived_func(17)
-
-    def test_case_18(self):
+    def test_case_1(self):
         contrived_func(18)
 
-    def test_case_19(self):
-        contrived_func(19)
-
-    def test_case_20(self):
-        contrived_func(20)
-
-    def test_case_21(self):
+    def test_case_1(self):
         contrived_func(21)
 
-    def test_case_22(self):
+    def test_case_1(self):
         contrived_func(22)
 
-    def test_case_23(self):
-        contrived_func(23)
-
-    def test_case_24(self):
-        contrived_func(24)
-
-    def test_case_25(self):
+     def test_case_1(self):
         contrived_func(25)
 
-    def test_case_26(self):
-        contrived_func(26)
-
-    def test_case_27(self):
-        contrived_func(27)
-
-    def test_case_28(self):
-        contrived_func(28)
-
-    def test_case_29(self):
-        contrived_func(29)
-
-    def test_case_30(self):
-        contrived_func(30)
 
 
 if __name__ == '__main__':
